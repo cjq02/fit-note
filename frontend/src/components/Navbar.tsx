3f96887a43be6c6c09e653e9975976eff64e6fe7
cjq <cjq02@qq.com>
2025-05-23 11:31:23 +0800

    提交

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,35 +1,43 @@
-import { Layout, Menu } from 'antd';
-import { HomeOutlined, PlusOutlined, UnorderedListOutlined } from '@ant-design/icons';
-import { Link, useLocation } from 'react-router-dom';
-
-const { Header } = Layout;
+import { TabBar } from 'antd-mobile';
+import { AppOutline, UnorderedListOutline, AddOutline, UserOutline } from 'antd-mobile-icons';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 export const Navbar = () => {
+    const navigate = useNavigate();
     const location = useLocation();
 
+    const tabs = [
+        {
+            key: '/',
+            title: '首页',
+            icon: <AppOutline />,
+        },
+        {
+            key: '/workout',
+            title: '训练记录',
+            icon: <UnorderedListOutline />,
+        },
+        {
+            key: '/workout/new',
+            title: '新增训练',
+            icon: <AddOutline />,
+        },
+        {
+            key: '/profile',
+            title: '我的',
+            icon: <UserOutline />,
+        },
+    ];
+
     return (
-        <Header className="bg-white shadow">
-            <Menu
-                mode="horizontal"
-                selectedKeys={[location.pathname]}
-                items={[
-                    {
-                        key: '/',
-                        icon: <HomeOutlined />,
-                        label: <Link to="/">首页</Link>,
-                    },
-                    {
-                        key: '/workout',
-                        icon: <UnorderedListOutlined />,
-                        label: <Link to="/workout">训练记录</Link>,
-                    },
-                    {
-                        key: '/workout/new',
-                        icon: <PlusOutlined />,
-                        label: <Link to="/workout/new">新增训练</Link>,
-                    },
-                ]}
-            />
-        </Header>
+        <TabBar
+            activeKey={location.pathname}
+            onChange={(key) => navigate(key)}
+            style={{ position: 'fixed', bottom: 0, left: 0, width: '100%', zIndex: 100, boxShadow: '0 -1px 6px #eee' }}
+        >
+            {tabs.map((item) => (
+                <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+            ))}
+        </TabBar>
     );
 }; 
